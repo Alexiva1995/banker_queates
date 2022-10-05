00ca4d5eff19b9d481bc678596c0f6b5ed284112
CésarF97 <febrescesar7@gmail.com>
2022-10-05 10:03:58 -0400

    Se agrego información adicional a los seeders de las licencias

--- database/migrations/2021_05_03_195750_create_licenses_packages_table.php
+++ database/migrations/2021_05_03_195750_create_licenses_packages_table.php
@@ -16,8 +16,13 @@ class CreateLicensesPackagesTable extends Migration
         Schema::create('licenses_packages', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->double('amount');
-            $table->double('rentability');
+            $table->double('amount')->comment('El precio al que se vende la licencia');
+            $table->enum('commissions', [0,1])->comment('Permite saber si genera o no comissiones (bonos) 0 - No genera, 1 - Si Genera');
+            $table->double('leadership_points')->comment('Puntos de liderazgo de la licencia')->nullable();
+            $table->double('binary_points')->comment('Puntos binarios de la licencia')->nullable();
+            $table->integer('level')->comment('El nivel maximo en red a los que permite acceso la licencia');
+            $table->double('deposit_min')->comment('El deposito minimo que permite la licencia')->nullable();
+            $table->double('deposit_max')->comment('El deposito maximo que permite la licencia')->nullable();
             $table->string('image')->nullable();
             $table->string('dark_image')->nullable();
             $table->timestamps();
--- database/seeders/LicensesPackagesSeeder.php
+++ database/seeders/LicensesPackagesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\LicensePackage;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -18,47 +19,80 @@ class LicensesPackagesSeeder extends Seeder
             [   
                 'name' => 'Consultant Binary Position',
                 'amount' => 50, 
-                'rentability' => 0, 
+                'commissions' => '0',
+                'leadership_points' => 0,
+                'binary_points' => 0,
+                'level' => 0,
+                'deposit_min' => 100,
+                'deposit_max' => 500,
                 'image' => null, 
                 'dark_image' => null
             ],
             [   
                 'name' => 'Standard License',
                 'amount' => 150, 
-                'rentability' => 0, 
+                'commissions' => '1',
+                'leadership_points' => 75,
+                'binary_points' => 75,
+                'level' => 3,
+                'deposit_min' => 500,
+                'deposit_max' => 1000, 
                 'image' => null, 
                 'dark_image' => null
             ],
             [   
                 'name' => 'Gold License.',
                 'amount' => 500, 
-                'rentability' => 0, 
+                'commissions' => '1',
+                'leadership_points' => 250,
+                'binary_points' => 250,
+                'level' => 4,
+                'deposit_min' => 1001,
+                'deposit_max' => 3000, 
                 'image' => null, 
                 'dark_image' => null
             ],
             [   
                 'name' => 'Titanium License',
                 'amount' => 1000, 
-                'rentability' => 0, 
+                'commissions' => '1',
+                'leadership_points' => 500,
+                'binary_points' => 500,
+                'level' => 5,
+                'deposit_min' => 3001,
+                'deposit_max' => 10000, 
                 'image' => null, 
                 'dark_image' => null
             ],
             [   
                 'name' => 'Platinum License',
                 'amount' => 2000, 
-                'rentability' => 0, 
+                'commissions' => '1',
+                'leadership_points' => 1000,
+                'binary_points' => 1000,
+                'level' => 7,
+                'deposit_min' => 10001,
+                'deposit_max' => 50000, 
                 'image' => null, 
                 'dark_image' => null
             ],
             [   
                 'name' => 'Banker Platinum License',
                 'amount' => 5000, 
-                'rentability' => 0, 
+                'commissions' => '1',
+                'leadership_points' => 2500,
+                'binary_points' => 2500,
+                'level' => 10,
+                'deposit_min' => 10001,
+                'deposit_max' => 100000, 
                 'image' => null, 
                 'dark_image' => null
             ],
         ];
 
-        DB::table('licenses_packages')->insert($licenses);
+        foreach($licenses as $license)
+        {
+            LicensePackage::create($license);
+        }
     }
 }
