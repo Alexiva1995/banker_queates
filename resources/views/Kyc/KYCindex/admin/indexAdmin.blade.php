1da73e45a2767c484ac63fc88994c6d90c37b26f
julio jimenez <Juliocesarzn5@gmail.com>
2022-10-07 10:36:35 -0400

    Update indexAdmin.blade.php

--- resources/views/Kyc/KYCindex/admin/indexAdmin.blade.php
+++ resources/views/Kyc/KYCindex/admin/indexAdmin.blade.php
@@ -12,7 +12,7 @@
             </div>
                 <div class="col-sm-12">
                     <div class="table-responsive" id="table">
-                        <table class="table nowrap scroll-horizontal-vertical myTable " id="myTable">
+                        <table class="table " id="myTable">
                             <thead class="">
                                 <tr class="text-center text-white bg-purple-alt2">
                                     <th>ID</th>
@@ -28,17 +28,29 @@
                                         <td>{{ $K->user_id }}</td>
                                        <td>{{'usuario'}}</td>
                                         <td>{{ $K->type_kyc }}</td>
-                                        <td class="d-flex justify-content-center">
-                                           
-                                            <button style="font-size: 10px" class="btn btn-success me-1"  type="button" data-bs-toggle="modal" data-bs-target="#frontModal{{ $K->user_id }}"><i class="fa fa-address-card" aria-hidden="true"></i> Frontal del documento</button>
-                                            <button class="btn btn-info"  type="button"  data-bs-toggle="modal" data-bs-target="#frontModal{{ $K->user_id }}"><i class="fa fa-credit-card" aria-hidden="true"></i> Espalda del documento</button>
+                                        <td class="">
+                                            <div class="row">
+                                                <div class="col-sm-6">
+                                                    <button style="font-size: 10px" class="btn btn-success me-1"  type="button" data-bs-toggle="modal" data-bs-target="#frontModal{{ $K->user_id }}"><i class="fa fa-address-card" aria-hidden="true"></i> Frontal del documento</button>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <button style="font-size: 10px" class="btn btn-info"  type="button"  data-bs-toggle="modal" data-bs-target="#frontModal{{ $K->user_id }}"><i class="fa fa-credit-card" aria-hidden="true"></i> Espalda del documento</button>
+                                                </div>
+                                                
+                                              </div>
                                         </td>
 
                                         <td>
                                             <form method="POST" action="{{route('KYC-accion')}}">
                                                 @csrf
-                                                <button type="submit" name="aprovar" value="{{ $K->user_id  }}" class="btn btn-success me-1"><i class="fa fa-check" aria-hidden="true" ></i> Aprobar</button>
-                                                <button type="submit" name="cancelar" value="{{ $K->user_id  }}" class="btn btn-danger"><i class="fa fa-ban" aria-hidden="true"></i> Cancelar</button>
+                                                <div class="row">
+                                                    <div class="col-sm-6">
+                                                        <button style="font-size: 10px" type="submit" name="aprovar" value="{{ $K->user_id  }}" class="btn btn-success me-1"><i class="fa fa-check" aria-hidden="true" ></i> Aprobar</button>
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <button style="font-size: 10px" type="submit" name="cancelar" value="{{ $K->user_id  }}" class="btn btn-danger"><i class="fa fa-ban" aria-hidden="true"></i> Cancelar</button>
+                                                    </div>
+                                                  </div>
                                             </form>
                                         </td>
 
