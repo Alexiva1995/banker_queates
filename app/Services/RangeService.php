79311558da5d16d36a6160a8b289e2c45315c39f
CésarF97 <febrescesar7@gmail.com>
2022-10-21 13:42:23 -0400

    Avance sistema de rangos: Asignar rango 1 Consultand

--- app/Services/RangeService.php
+++ app/Services/RangeService.php
@@ -17,40 +17,51 @@ class RangeService
     public function setUserRanges()
     {
         // Traemos a todos los usuarios del sistema menos al admin
-        $users = User::where('status', '1')->where('id', '!=', '1')->get();
         // No nos interesan los usuarios con rango max, asi que los obviamos
-        $users = $users->where('range_id', '!=', 3);
+        $users = User::where('status', '1')
+                        ->with('referidos')
+                        ->with('range')
+                        ->where('id', '!=', '1')
+                        ->where('range_id', '!=', 6)
+                        ->get();
 
-        foreach ($users as $user) {
-            $countReferrals = 0;
-            foreach ($user->referidos as $referido) {
-                foreach ($referido->investments as $investment) {
-                    if ($investment->invested == 7000) {
-                        if ($countReferrals > 3) {
-                            // Se pregunta si la inversion de este referido esta dentro de los 60 dias
-                            if ($investment->updated_at > now()->subDays(60)) {
-                                $countReferrals++;
-                                // Validación bono DOBLE DIAMANTE
-                                if ($countReferrals == 7) {
-                                    $user->range_id = 2;
-                                }
-                                // Validación bono TRIPLE DIAMANTE
-                                if ($countReferrals == 14) {
-                                    $user->range_id = 3;
-                                }
-                            }
-                        // Se pregunta si la inversion de este referido esta dentro de los 30 dias
-                        } else if ($investment->updated_at > now()->subDays(30)) {
-                            $countReferrals++;
-                            // Validación bono DIAMANTE
-                            if ($countReferrals == 3) {
-                                $user->range_id = 1;
-                            }
-                        }
-                        $user->save();
-                    }
-                }
-            }
+        $usersWithOutRange = $users->where('range_id', null);
+
+        $usersWithConsultantRange = $users->where('range_id', 1);
+
+        foreach ($usersWithOutRange as $user) {
+            $this->consultantRange($user);
+        }
+
+
+    }
+    /**
+     * Verifica si el usuario es apto para el rango 1 - Consultant y lo asigna. 
+     * Para obtener este rango el usuario debe tener un afiliado que 
+     */
+    private function consultantRange(User $user) 
+    {
+        foreach($user->referidos as $children) 
+        {
+            if($children->hasActiveLicense()) {
+                $user->update(['range_id' => 1]);
+                break;
+            };
         }
     }
+    /**
+     * Verifica si el usuario es apto para el rango 2 - Qualidied Consultant y lo asigna. 
+     * Para obtener este rango el usuario debe tener un referido por cada lado
+     */
+    private function qualifiedConsultantRange(User $user) 
+    {
+        foreach($user->referidos as $children) 
+        {
+            if($children->hasActiveLicense()) {
+                $user->update(['range_id' => 1]);
+                break;
+            };
+        }
+    }
+
 }
