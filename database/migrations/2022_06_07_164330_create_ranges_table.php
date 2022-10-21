f20700563d453c1caf2c84c923f9c89f143d37aa
CésarF97 <febrescesar7@gmail.com>
2022-10-21 11:36:14 -0400

    Inicio Sistema de Rangos: Migraciones, Modelos, Relaciones y Seeders

--- database/migrations/2022_06_07_164330_create_ranges_table.php
+++ database/migrations/2022_06_07_164330_create_ranges_table.php
@@ -16,6 +16,7 @@ class CreateRangesTable extends Migration
         Schema::create('ranges', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
