af15111d70813234b0b555e5e32c763f9f9c75be
CésarF97 <febrescesar7@gmail.com>
2022-10-21 11:12:31 -0400

    Ultimos cambios modulo binario

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -54,7 +54,6 @@ class Kernel extends ConsoleKernel
         $schedule->command('bonus:range')->monthlyOn(30, '00:00');
         $schedule->command('bonos:pamm')->everyMinute();
         $schedule->command('delete:binary:points')->daily();
-        // TODO:: cambiar fecha del cron
         $schedule->command('corte:ganancias:binarias')->daily();
 
     }
