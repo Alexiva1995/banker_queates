07c74ebc3629232c0199ce9c0d95b2d1aca42b1e
CésarF97 <febrescesar7@gmail.com>
2022-10-21 14:51:14 -0400

    Avance sistema de rangos: Asignar rango  2 Qualified Consultand

--- app/Services/RangeService.php
+++ app/Services/RangeService.php
@@ -17,23 +17,15 @@ class RangeService
     public function setUserRanges()
     {
         // Traemos a todos los usuarios del sistema menos al admin
+        $users = User::where('status', '1')->with('referidos', 'range', 'binaryChildrens')->where('id', '!=', '1')->get();
+
         // No nos interesan los usuarios con rango max, asi que los obviamos
-        $users = User::where('status', '1')
-                        ->with('referidos')
-                        ->with('range')
-                        ->where('id', '!=', '1')
-                        ->where('range_id', '!=', 6)
-                        ->get();
+        $users = $users->where('range_id', '!=', 6);
 
-        $usersWithOutRange = $users->where('range_id', null);
-
-        $usersWithConsultantRange = $users->where('range_id', 1);
-
-        foreach ($usersWithOutRange as $user) {
+        foreach ($users as $user) 
+        {
             $this->consultantRange($user);
         }
-
-
     }
     /**
      * Verifica si el usuario es apto para el rango 1 - Consultant y lo asigna. 
@@ -43,24 +35,38 @@ class RangeService
     {
         foreach($user->referidos as $children) 
         {
-            if($children->hasActiveLicense()) {
+            if($children->hasActiveLicense()) 
+            {
                 $user->update(['range_id' => 1]);
+                // Como obtuvo el rango 1 se envia evaluar al rango 2 - Qualified Consultant
+                $this->qualifiedConsultantRange($user);
                 break;
             };
         }
     }
     /**
-     * Verifica si el usuario es apto para el rango 2 - Qualidied Consultant y lo asigna. 
+     * Verifica si el usuario es apto para el rango 2 - Qualified Consultant y lo asigna. 
      * Para obtener este rango el usuario debe tener un referido por cada lado
      */
     private function qualifiedConsultantRange(User $user) 
     {
-        foreach($user->referidos as $children) 
+        $left_right = false;
+        $right_side = false;
+
+        foreach($user->binaryChildrens as $children) 
         {
-            if($children->hasActiveLicense()) {
-                $user->update(['range_id' => 1]);
-                break;
+            Log::debug($children);
+            if($children->binary_side === 'L') {
+                $left_right = true;
             };
+
+            if($children->binary_side === 'R') {
+                $right_side = true;
+            }
+        }
+        
+        if( $left_right && $right_side ) {
+            $user->update(['range_id' => 2]);
         }
     }
 
