83abb9292e2d3304e743c4a2163782d0e139946a
Ashley <ashleysanchez0510@gmail.com>
2022-11-17 09:43:19 -0400

    MOD solucionando el error 500 en red unilevel

--- resources/views/unilevel/levels/level6.blade.php
+++ resources/views/unilevel/levels/level6.blade.php
@@ -24,15 +24,16 @@
             
                                         <td>{{ $user->username }}</td>
                                         <td>{{ $user->email }}</td>
+                                        @if($user->investment != null)
+                                            <td>{{$user->investment->LicensePackage->name}}</td>
+                                        @else
+                                            <td>No tiene licencias activas</td>
+                                        @endif
             
                                         @if ($user->investments->count() > 0)
-                                            <td>{{ $user->investments->last()->membershipPackage->membershipType->name }}
-                                            </td>
-                                            <td class="text-end">{{ number_format($user->investments->last()->invested, 2, ',', '.') }}
-                                            </td>
                                             @if ($user->investments->last()->status == 0)
                                                 <td class="text-center">
-                                                    <span class="badge bg-wawning">En espera</span>
+                                                    <span class="badge bg-warning">En espera</span>
                                                 </td>
                                             @elseif ($user->investments->last()->status == 1)
                                                 <td class="text-center">
@@ -44,7 +45,6 @@
                                                 </td>
                                             @endif
                                         @else
-                                            <td>No tiene licencias activas</td>
                                             <td>-</td>
                                         @endif
                                         <td>{{ $user->padre->fullName() }}</td>
