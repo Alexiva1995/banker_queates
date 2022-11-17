332299f15229e65b0d343943f8f2246164ebad85
Kevin Pedraza <kdpedrazaj@gmail.com>
2022-11-17 09:37:39 -0400

    MOD-CorreccionesVarias-ModuloTickets

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -78,13 +78,13 @@
 
 <nav class="col-sm-12 lista">
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <button class="nav-link" id="nav-rentability-tab" data-bs-toggle="tab" data-bs-target="#general"
+        <button class="nav-link active" id="nav-rentability-tab" data-bs-toggle="tab" data-bs-target="#general"
         type="button" role="tab" aria-controls="nav-rentability" aria-selected="false">General</button>
 
         <button class="nav-link" id="nav-rentability-tab" data-bs-toggle="tab" data-bs-target="#license"
         type="button" role="tab" aria-controls="nav-rentability" aria-selected="false">Licencias</button>
 
-        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button"
+        <button class="nav-link" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button"
         role="tab" aria-controls="nav-home" aria-selected="true">MLM PAMM</button>
 
         <button class="nav-link" id="nav-range-tab" data-bs-toggle="tab" data-bs-target="#nav-range" type="button"
