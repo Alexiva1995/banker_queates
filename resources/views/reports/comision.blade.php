778845932210f699dffea3565434017668a6e198
Kevin Pedraza <kdpedrazaj@gmail.com>
2022-11-28 19:11:22 -0400

    MOD - Tabla Comisiones Arreglo - Modulo Informes

--- resources/views/reports/comision.blade.php
+++ resources/views/reports/comision.blade.php
@@ -47,7 +47,6 @@
                                             <th class="fw-600">ID del <br/>Referido</th>
                                         @endif
                                         <th class="fw-600">Monto</th>
-                                        <th class="fw-600">Comision</th>
                                         <th class="fw-600">Estado</th>
                                         <th class="fw-600">Tipo</th>
                                         <th class="fw-600">Fecha</th>
@@ -64,7 +63,6 @@
                                         <td class="fw-300 text-center">{{$wallet->buyer->name ?? '--'}}</td>
                                         <td class="fw-300 text-center">{{$wallet->buyer->id ?? '--'}}</td>
                                         <td class="fw-300 text-end">{{number_format($wallet->amount,2)}}</td>
-                                        <td class="fw-300 text-end">{{number_format($wallet->amount - $wallet->amount_available,2)}}</td>
                                         @if ($wallet->status == 0)
                                             <td class="fw-300 text-center"> <a class=" btn bg-light-info  fw-300 p-75">Disponible</a></td>
                                         @elseif($wallet->status == 1)
